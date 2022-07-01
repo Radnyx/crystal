4f5f8ad14adcfe769d03cca986d5d216b9d006d3
Radnyx <radnyx.again@gmail.com>
2022-06-30 22:40:50 -0700

    Package index.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export * from "./BattleObjects";
+export * from "./BattleScript";
+export * from "./GameV2";
+export * from "./IResources";
+export * from "./Interpreter";
+export * as moveInfo from "./MoveInfo";
+export * from "./View";
+export * from "./Event";
+export * as Graphics from "./Graphics";
