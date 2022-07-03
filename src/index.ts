55363be4dd0f864a232fdbe53cfc67c0ec58438c
Radnyx <radnyx.again@gmail.com>
2022-07-03 12:59:23 -0700

    Fix index.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import View from "./View";
+import BattleScript from "./BattleScript";
+import GameV2 from "./GameV2";
+import IResources from "./IResources";
+import Interpreter from "./Interpreter";
+
 export * from "./BattleObjects";
-export * from "./BattleScript";
-export * from "./GameV2";
-export * from "./IResources";
-export * from "./Interpreter";
 export * as moveInfo from "./MoveInfo";
 export * from "./View";
 export * from "./Event";
+export * from "./Script";
 export * as Graphics from "./Graphics";
 export * as Input from "./Input";
+export { View, BattleScript, GameV2, IResources, Interpreter };
